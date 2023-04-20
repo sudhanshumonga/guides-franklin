9423e7e6f23ed17684d5bfa86b6d18e2bebc9659
smonga <smonga@adobe.com>
2023-04-20 11:29:46 +0530

    commented out scripts

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,7 +1,7 @@
-// eslint-disable-next-line import/no-cycle
-import { sampleRUM } from './lib-franklin.js';
+// // eslint-disable-next-line import/no-cycle
+// import { sampleRUM } from './lib-franklin.js';
 
-// Core Web Vitals RUM collection
-sampleRUM('cwv');
+// // Core Web Vitals RUM collection
+// sampleRUM('cwv');
 
-// add more delayed functionality here
+// // add more delayed functionality here

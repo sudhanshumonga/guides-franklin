393392e20ad562faf50efb3df8a86de153bba9c1
smonga <smonga@adobe.com>
2023-05-11 16:42:10 +0530

    now showing the click span only in case there are children

--- blocks/sidenav/sidenav.js
+++ blocks/sidenav/sidenav.js
@@ -1,40 +1,45 @@
-import treeData from './sidenav_data.js'
+import treeData from "./sidenav_data.js";
 
 function createTree(parent, data) {
-    const ul = document.createElement("ul");
-    ul.classList.add("tree");
-    parent.appendChild(ul);
-    data.forEach((item) => {
-      const li = document.createElement("li");
-      ul.appendChild(li);
-      const anchor = document.createElement("a");
-      const span = document.createElement("span");
-      span.style.width = '10px'
-      span.style.height = '10px'
-      span.style.minWidth = '10px'
-      span.style.display = 'flex'
-      span.style.backgroundColor = 'black'
-      anchor.textContent = item.displayName;
-      anchor.setAttribute('href', `https://main--guides_franklin--sudhanshumonga.hlx.page/${item.url}`)
-      li.classList.add('sidenav-list-item')
-      li.appendChild(span);
-      li.appendChild(anchor);
-      if (item.children) {
+  const ul = document.createElement("ul");
+  ul.classList.add("tree");
+  parent.appendChild(ul);
+  data.forEach((item) => {
+    const li = document.createElement("li");
+    ul.appendChild(li);
+    const anchor = document.createElement("a");
+    const span = document.createElement("span");
+    span.style.width = "10px";
+    span.style.height = "10px";
+    span.style.minWidth = "10px";
+    span.style.display = "flex";
+    span.style.backgroundColor = "black";
+    anchor.textContent = item.displayName;
+    if (item.url) {
+      anchor.setAttribute(
+        "href",
+        `https://main--guides_franklin--sudhanshumonga.hlx.page/${item.url}`
+      );
+    }
+    li.classList.add("sidenav-list-item");
+    if (item.children) {
+        li.appendChild(span);
         createTree(li, item.children);
-      } else {
+    } else {
+        li.appendChild(anchor);
         li.classList.add("closed");
-      }
-    });
-  }
-  
-  // Get the treeview element and create the tree
-  const treeview = document.getElementsByClassName("sidenav")[0];
-  createTree(treeview, treeData);
-  
-  // Add click event listener to each span element
-  treeview.querySelectorAll("span").forEach((span) => {
-    span.addEventListener("click", (event) => {
-      // Toggle the "closed" class on the parent li element
-      event.currentTarget.parentNode.classList.toggle("closed");
-    });
+    }
   });
+}
+
+// Get the treeview element and create the tree
+const treeview = document.getElementsByClassName("sidenav")[0];
+createTree(treeview, treeData);
+
+// Add click event listener to each span element
+treeview.querySelectorAll("span").forEach((span) => {
+  span.addEventListener("click", (event) => {
+    // Toggle the "closed" class on the parent li element
+    event.currentTarget.parentNode.classList.toggle("closed");
+  });
+});

66685d4988ac0c6486acd05805b204088c0e2367
smonga <smonga@adobe.com>
2023-05-15 16:05:25 +0530

    expansion remembered across navigation

--- blocks/sidenav/sidenav.js
+++ blocks/sidenav/sidenav.js
@@ -1,4 +1,22 @@
 import treeData from "./sidenav_data.js";
+let id = 0
+
+function expandHeirarchy(element, root) {
+    if(element === root) return
+    let parent = element.parentElement
+    parent.classList.remove('closed')
+    expandHeirarchy(parent, root)
+}
+
+function expandSelection(parent) {
+    let queryString = window.location.search;
+    let params = new URLSearchParams(queryString);
+    let id = params.get('expand')
+    let element = parent.getElementById(id)
+    element.classList.add('selected')
+    expandHeirarchy(element, parent)
+    element.scrollIntoView();
+}
 
 function createTree(parent, data) {
   const ul = document.createElement("ul");
@@ -6,17 +24,22 @@ function createTree(parent, data) {
   parent.appendChild(ul);
   data.forEach((item) => {
     const li = document.createElement("li");
+    li.setAttribute("id", id++)
     ul.appendChild(li);
     const anchor = document.createElement("a");
     const span = document.createElement("span");
     span.classList.add('chevron-icon-span')
     span.textContent = '>'
     anchor.textContent = item.displayName;
+    const siteURL = window.location.protocol + '//' + window.location.hostname + (window.location.port ? ':' + window.location.port : '');
     if (item.url) {
       anchor.setAttribute(
         "href",
-        `https://main--guides_franklin--sudhanshumonga.hlx.page/${item.url}`
+        `${siteURL}/${item.url}`
       );
+      anchor.addEventListener('click', () => {
+        onClick(id - 1)
+      })
     }
     li.classList.add("sidenav-list-item");
     li.classList.add("closed");
@@ -33,9 +56,17 @@ function createTree(parent, data) {
   });
 }
 
+function onClick(id) {
+    let queryString = window.location.search;
+    let params = new URLSearchParams(queryString);
+    params.set('expand', id);
+    window.location.search = params.toString();
+}
+
 // Get the treeview element and create the tree
 const treeview = document.getElementsByClassName("sidenav")[0];
 createTree(treeview, treeData);
+expandSelection(treeview)
 
 // Add click event listener to each span element
 treeview.querySelectorAll("span").forEach((span) => {

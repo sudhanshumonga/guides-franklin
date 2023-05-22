9609a8d05a68bf4accdad7e474af2078a752a18d
smonga <smonga@adobe.com>
2023-05-22 17:04:40 +0530

    styled minitoc block

--- blocks/minitoc/minitoc.js
+++ blocks/minitoc/minitoc.js
@@ -1,6 +1,8 @@
 var content = document.querySelector('body')
 var headings = content.querySelectorAll("h1, h2, h3, h4, h5, h6");
 var minitocContainer = content.getElementsByClassName("minitoc")[0];
+var textNode = document.createTextNode("In this topic")
+minitocContainer.appendChild(textNode)
 var miniTOCList = document.createElement("ul");
 minitocContainer.appendChild(miniTOCList)
 miniTOCList.classList.add('minitoc-list')

0a907e698e6eb4ed08602db0c38d22b22e85129d
smonga <smonga@adobe.com>
2023-05-22 16:46:36 +0530

    appened as a child to minitoc conntainer

--- blocks/minitoc/minitoc.js
+++ blocks/minitoc/minitoc.js
@@ -2,6 +2,7 @@ var content = document.querySelector('body')
 var headings = content.querySelectorAll("h1, h2, h3, h4, h5, h6");
 var minitocContainer = content.getElementsByClassName("minitoc")[0];
 var miniTOCList = document.createElement("ul");
+minitocContainer.appendChild(miniTOCList)
 miniTOCList.classList.add('minitoc-list')
 for (var i = 0; i < headings.length; i++) {
   var heading = headings[i];

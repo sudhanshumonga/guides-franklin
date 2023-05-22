23b501a9148e324ad067256fd22b4ff2865c2b16
smonga <smonga@adobe.com>
2023-05-22 16:44:05 +0530

    fixed a typo

--- blocks/minitoc/minitoc.js
+++ blocks/minitoc/minitoc.js
@@ -1,6 +1,6 @@
 var content = document.querySelector('body')
 var headings = content.querySelectorAll("h1, h2, h3, h4, h5, h6");
-var minitocContainer = content.getElementById("minitoc");
+var minitocContainer = content.getElementsByClassName("minitoc")[0];
 var miniTOCList = document.createElement("ul");
 miniTOCList.classList.add('minitoc-list')
 for (var i = 0; i < headings.length; i++) {

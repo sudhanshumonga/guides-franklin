b7ef5dbf6a24f08d0c0a4b89a9cc4d6cecaa7c01
Sudhanshu Monga <sudhanshumonga@users.noreply.github.com>
2023-07-05 18:19:28 +0530

    uploading sidenav to: https://api.github.com/repos/sudhanshumonga/guides-franklin/contents/blocks/sidenav/sidenav.js

--- blocks/sidenav/sidenav.js
+++ blocks/sidenav/sidenav.js
@@ -1,5 +1,5 @@
 import { migrateTree } from "../utils.js";
-const treeData = [{"displayName":"Personal Spaceship Operations Manual","url":"/content/dam/guides-regression/TravelDITA/Cover/personal-spaceship-operations-manual"},{"displayName":"Legal","url":"/content/dam/guides-regression/TravelDITA/Legal/legal"},{"displayName":"Contacts","url":"/content/dam/guides-regression/TravelDITA/Contacts/contacts"}]
+const treeData = [{"displayName":"Personal Spaceship Operations Manual","url":"/content/dam/guides-regression/TravelDITA/Cover/personal-spaceship-operations-manual"},{"displayName":"Legal","url":"/content/dam/guides-regression/TravelDITA/Legal/legal"},{"displayName":"contacts.xml","url":"/content/dam/guides-regression/TravelDITA/Contacts/contacts"}]
 
 let id = 0;
 

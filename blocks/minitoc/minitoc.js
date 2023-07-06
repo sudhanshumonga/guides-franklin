71261a652345592070d3cad763f5bab4954aa9c0
smonga <smonga@adobe.com>
2023-07-06 15:25:12 +0530

    added taglist array

--- blocks/minitoc/minitoc.js
+++ blocks/minitoc/minitoc.js
@@ -2,6 +2,7 @@ var content = document.querySelector('body')
 var headings = content.querySelectorAll("h1, h2, h3, h4, h5, h6");
 var minitocContainer = content.getElementsByClassName("minitoc")[0];
 var miniTOCList = document.createElement("ul");
+const tagList = []
 for (var i = 0; i < headings.length; i++) {
   var heading = headings[i];
   var headingLevel = parseInt(heading.tagName[1]);
@@ -12,6 +13,7 @@ for (var i = 0; i < headings.length; i++) {
   link.href = "#" + heading.id;
   listItem.appendChild(link);
   miniTOCList.appendChild(listItem);
+  tagList.push(listItem)
 }
 if(headings.length !== 0) {
     var h2minitocText = document.createElement('h2')

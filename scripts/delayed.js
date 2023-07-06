3c4a14373c4cf0286bf433cca27a2d067986d976
smonga <smonga@adobe.com>
2023-07-06 16:00:46 +0530

    moved custom event in delayed.js

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -3,5 +3,6 @@ import { sampleRUM } from './lib-franklin.js';
 
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
-
+const customEvent = new Event('franklin-website-ready');
+window.dispatchEvent(customEvent)
 // add more delayed functionality here

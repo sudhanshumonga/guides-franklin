20bdf73bd0fc5b301fa0c1e53531bf679f0fd67b
Sudhanshu Monga <sudhanshumonga@users.noreply.github.com>
2023-07-21 16:53:40 +0530

    uploading sidenav to: https://api.github.com/repos/sudhanshumonga/guides-franklin/contents/blocks/sidenav/sidenav.js

--- blocks/sidenav/sidenav.js
+++ blocks/sidenav/sidenav.js
@@ -1,5 +1,5 @@
 import { migrateTree } from "../utils.js";
-const treeData = [{"displayName":"GUID-a7cd8341-d925-4278-9469-cc9316195493.ditamap","url":"/content/dam/pdf/ditamap-1-flattened/guid-a7cd8341-d925-4278-9469-cc9316195493"}]
+const treeData = [{"displayName":"Topic is sample","url":"/content/dam/dita-templates/maps/topic"}]
 
 let id = 0;
 

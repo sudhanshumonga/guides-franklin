d67f52405ff62db1c36157668b064fa8d30fa21b
Sudhanshu Monga <smonga@adobe.com>
2023-10-29 15:46:21 +0530

    minitoc scroll behaviour changed

--- blocks/minitoc/minitoc.js
+++ blocks/minitoc/minitoc.js
@@ -36,9 +36,9 @@ window.addEventListener('franklin-app-ready', () => {
   function isInViewport(element) {
     const rect = element.getBoundingClientRect();
     return (
-      rect.top >= 0 &&
+      rect.top >= 100 &&
       rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.bottom <= (window.innerHeight || (document.documentElement.clientHeight - 100)) &&
       rect.right <= (window.innerWidth || document.documentElement.clientWidth)
     );
   }
@@ -65,7 +65,8 @@ window.addEventListener('franklin-app-ready', () => {
       }, 0)
     })
   })
-  window.addEventListener('scroll', updateSelectedTag);
+  const contentSection = document.querySelector('.section.breadcrumbs-container')
+  contentSection.addEventListener('scroll', updateSelectedTag);
   window.addEventListener('resize', updateSelectedTag);
   updateSelectedTag()
 });

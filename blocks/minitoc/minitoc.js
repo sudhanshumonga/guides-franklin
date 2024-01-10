c36d43ca7b07b1de49de407b86c21a5c1be66af3
Sudhanshu Monga <smonga@adobe.com>
2024-01-10 13:26:20 +0530

    added the notion of scroll reaching top and bottom for center panel

--- blocks/minitoc/minitoc.js
+++ blocks/minitoc/minitoc.js
@@ -11,7 +11,7 @@ for (var i = 0; i < headings.length; i++) {
   listItem.classList.add(`minitoc-level-${headingLevel}`)
   var link = document.createElement("a");
   link.textContent = heading.textContent;
-  if(!heading.textContent) {
+  if (!heading.textContent) {
     continue
   }
   link.href = "#" + heading.id;
@@ -19,20 +19,25 @@ for (var i = 0; i < headings.length; i++) {
   miniTOCList.appendChild(listItem);
   tagList.push(listItem)
 }
-if(tagList.length !== 0) {
-    var h2minitocText = document.createElement('h2')
-    h2minitocText.classList.add('minitoc-title')
-    var textNode = document.createTextNode("In this article")
-    miniTOCList.classList.add('minitoc-list')
-    h2minitocText.appendChild(textNode)
-    minitocContainer.appendChild(h2minitocText)
-    minitocContainer.appendChild(miniTOCList)
+if (tagList.length !== 0) {
+  var h2minitocText = document.createElement('h2')
+  h2minitocText.classList.add('minitoc-title')
+  var textNode = document.createTextNode("In this article")
+  miniTOCList.classList.add('minitoc-list')
+  h2minitocText.appendChild(textNode)
+  minitocContainer.appendChild(h2minitocText)
+  minitocContainer.appendChild(miniTOCList)
 } else {
   const minitocContainerSection = document.getElementsByClassName('minitoc-container')[0]
   minitocContainerSection.classList.add('force-hide')
 }
 
 window.addEventListener('franklin-app-ready', () => {
+  const contentSection = document.querySelector('.section.breadcrumbs-container')
+
+  const body = document.querySelector('body')
+
+
   function isInViewport(element) {
     const rect = element.getBoundingClientRect();
     return (
@@ -42,19 +47,31 @@ window.addEventListener('franklin-app-ready', () => {
       rect.right <= (window.innerWidth || document.documentElement.clientWidth)
     );
   }
-  
-  function updateSelectedTag() {
+
+  function updateSelectedTag(arg1, arg2) {
     for (let i = 0; i < headings.length; i++) {
       if (isInViewport(headings[i])) {
         for (let j = 0; j < tagList.length; j++) {
           tagList[j].classList.remove('selected');
         }
-        
+
         tagList[i].classList.add('selected');
         break;
       }
     }
+
+    if (contentSection.scrollTop + contentSection.clientHeight >= contentSection.scrollHeight) {
+      // You have reached the bottom of the element
+      console.log("You have reached the bottom of the element!");
+    }
+
+    if (contentSection.scrollTop === 0) {
+      // You have reached the top of the element
+      console.log("You have reached the top of the element!");
   }
+
+  }
+
   tagList.forEach(tag => {
     tag.addEventListener('click', (event) => {
       event.preventDefault();
@@ -64,30 +81,33 @@ window.addEventListener('franklin-app-ready', () => {
         }
         tag.classList.add('selected');
         const a_tag = tag.children[0]
-        if(a_tag) {
+        if (a_tag) {
           let href = a_tag.attributes['href'].value
           let target_id
-          if(href) {
+          if (href) {
             target_id = href[0] === '#' ? href.substr(1, href.length - 1) : href
           }
           let targetNode
           for (var i = 0; i < headings.length; i++) {
             let heading = headings[i]
             let id = heading.attributes['id'].value
-            if(id === target_id) {
+            if (id === target_id) {
               targetNode = heading
               break;
             }
           }
           let offset = 300
-          if(targetNode) {
-            targetNode.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest', offsetTop: offset})
+          if (targetNode) {
+            targetNode.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest', offsetTop: offset })
           }
         }
       }, 0)
     })
   })
-  const contentSection = document.querySelector('.section.breadcrumbs-container')
+
+
+
+
   contentSection.addEventListener('scroll', updateSelectedTag);
   window.addEventListener('resize', updateSelectedTag);
   updateSelectedTag()

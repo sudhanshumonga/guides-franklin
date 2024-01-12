0162eadc569fdea27b7c454650aab71d2319fe7d
Sudhanshu Monga <smonga@adobe.com>
2024-01-12 13:43:47 +0530

    added style for sidenav for seo

--- blocks/sidenav/sidenav.js
+++ blocks/sidenav/sidenav.js
@@ -89,7 +89,7 @@ function createTree(parent, data, prefix, level) {
     ul.appendChild(li);
     let anchor = document.createElement("a");
     if(item.children) {
-      anchor = document.createElement("a");
+      anchor = document.createElement("span");
     }
     const span = document.createElement("span");
     span.classList.add("chevron-icon-span");
